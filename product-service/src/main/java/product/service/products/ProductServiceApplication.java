51f044bc261558002e851fb57005fbdbe2d05d7a
EAAR85 <44047823+EAAR85@users.noreply.github.com>
2021-08-27 20:39:06 -0500

    eaar

--- product-service/src/main/java/product/service/products/ProductServiceApplication.java
+++ product-service/src/main/java/product/service/products/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package product.service.products;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration()
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
